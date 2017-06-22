1980ce10702d143d9f05a844015e31d12dc2dd06
Blörg <devel@bloerg.de>
2017-06-22 09:25:20 +0200

    initial commit

---
+++ map/js/color_helpers.js
@@ -0,0 +1,24 @@
+
+function redshift_to_color(min_redshift, max_redshift, redshift) {
+
+    if ( redshift == null || redshift == -9999.0 ) return 'rgba(0,0,0,0)'
+    else {
+        var color = (redshift - min_redshift)  * 300 / (max_redshift - min_redshift); //normalize to violett (300)
+        return 'hsl(' + color + ',90%,50%)'
+    }
+}
+
+function sn_median_to_color(min_sn_median, max_sn_median, sn_median) {
+    if ( sn_median == null) return 'rgba(0,0,0,0)'
+    else {
+        var color = (sn_median - min_sn_median)  * 300 / (max_sn_median - min_sn_median); //normalize to violett (300)
+        return 'hsl(' + color + ',50%,50%)'
+    }
+}
+
+function spec_class_to_color(spec_class) {
+    if (spec_class == "S".charCodeAt(0)) return '#d95f02'
+    else if (spec_class == "G".charCodeAt(0)) return '#1b9e77'
+    else if (spec_class == "Q".charCodeAt(0)) return '#7570b3'
+    else return 'rgba(0,0,0,0)'
+}
