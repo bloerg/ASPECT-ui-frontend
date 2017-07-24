84026fe83615dafc9a45741733751b504847fa0d
Blörg <devel@bloerg.de>
2017-07-24 10:36:18 +0200

    get ajax function

--- map/js/plain.js
+++ map/js/plain.js
@@ -1,3 +1,16 @@
 function addEvent(el, type, handler) {
     if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
 }
+
+
+function getAjax(url, success, not_found) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+        if (xhr.readyState>3 && xhr.status==404) not_found();
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
+}
